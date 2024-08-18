f81dd3edca296ffa66192c1f7d71efd1720dc05b
Bittu kumar prajapati <109884531+Bittu-kumar2003@users.noreply.github.com>
2024-08-18 11:54:46 +0530

    Add files via upload

---
+++ color protofilo/js/script.js
@@ -0,0 +1,11 @@
+var typed = new Typed(".typing", {
+  Strings: [
+    "web Designer",
+    "Graphic Designer",
+    "web Designer",
+    "Graphic Designer",
+  ],
+  typeSpeed: 100,
+  BackSpeed: 60,
+  loop: true,
+});
